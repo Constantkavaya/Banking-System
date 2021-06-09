6cf1e2a4b9625c5f25a78bbb4fbee9fd436bfed9
Constantkavaya <kavayaconstant@gmail.comgit config --listgit config --global user.email kavayaconstant@gmail.com>
2021-06-09 21:09:24 +0300

    first commit

--- bank.py
+++ bank.py
@@ -1,3 +1,4 @@
+from datetime import  date, datetime, time
 class Account:
      def __init__(self,name,phone):
          self.name=name
@@ -7,13 +8,21 @@ class Account:
          self.loanlimit=34000
          self.loan=0
          self.loanfees=5 
+         self.transactions=[]
+         self.repay=0
+         self.repayed=[]
      def deposite(self,amount):
 
          if amount<=0:
-            print ("please doposite a valid amount")
+            print ("please deposite a valid amount")
 
          else:
           self.balance+=amount
+          transction={
+            "amount":amount,"balance":self.balance,"narration":"You have deposited",
+            "time":datetime.now(),}
+          
+          self.transactions.append(transction)
           return f"Hello {self.name} you have deposited{amount} your new balance is{self.balance}"
      def withdraw(self,withdraw_amount):
          total=withdraw_amount+self.transaction
@@ -26,7 +35,13 @@ class Account:
        
          else:
            self.balance==total
-           return f"hello {self.name} you  have successfully borrowed{withdraw_amount} and you account balance is{self.balance}"
+           transction={
+            "withdraw_amount":withdraw_amount,"balance":self.balance,"narration":"You have withdrawn",
+            "time":datetime.now(),}
+          
+           self.transactions.append(transction)
+           return f"Hello {self.name} you have deposited{withdraw_amount} your new balance is{self.balance}"
+           
      def borrow(self,borrow_amount) :
          loan=borrow_amount- self.loanlimit
          loanfees=0.05*borrow_amount
@@ -36,7 +51,55 @@ class Account:
              return "try again later"  
          else :
              self.balance=loan
-             return f"Hi {self.name} you are not allowed to borrow{borrow_amount} since you outstanding balance is{self.balance}"    
+             transction={
+            "borrow_amount":borrow_amount,"balance":self.balance,"narration":"You have borrowed",
+            "time":datetime.now(),}
+          
+             self.transactions.append(transction)
+           
+             return f"Hi {self.name} you are not allowed to borrow{borrow_amount} since you outstanding balance is{self.balance}" 
+
+     def get_statement(self):
+       for transaction in self.transactions:
+         amount=transaction["amount"]
+         narration=transaction["narration"]
+         balance=transaction["balance"]
+         time=transaction["time"]
+         date=time.strftime("%D")
+         print("f{date}...{narration}...{amount}...{balance}")
+     
+
+     def repay_loan(self,amount):
+       
+        if amount<0:
+          return "you  are not allowed to pay"
+        elif amount> self.loan:
+          minus=amount-self.loan
+          self.balance+=minus
+          transction={
+          "borrow_amount":amount,"balance":self.balance,"narration":"You have successfully repaid",
+          "time":datetime.now(),}
+          
+          self.transactions.append(transction)
+          return "you are awesome"
+        else:
+          self.loan-=amount
+          transction={
+          "borrow_amount":amount,"balance":self.balance,"narration":"You have successfully repaid",
+          "time":datetime.now(),}
+          
+          self.transactions.append(transction)
+          return "you have an existing loan balance"
+
+         
+
+        
+    
+     
+
+                
+
+
            
 
    
