7329440657ef32766604138cc5ac9d62184ca4c9
Constantkavaya <kavayaconstant@gmail.comgit config --listgit config --global user.email kavayaconstant@gmail.com>
2021-06-11 19:32:33 +0300

    first commit

--- bank.py
+++ bank.py
@@ -11,12 +11,17 @@ class Account:
          self.transactions=[]
          self.repay=0
          self.repayed=[]
-     def deposite(self,amount):
 
-         if amount<=0:
+     def deposite(self,amount):
+        try:
+          amount+10
+        except TypeError:
+          return f"please enter amount in figures"
+
+        if amount<=0:
             print ("please deposite a valid amount")
 
-         else:
+        else:
           self.balance+=amount
           transction={
             "amount":amount,"balance":self.balance,"narration":"You have deposited",
@@ -25,31 +30,40 @@ class Account:
           self.transactions.append(transction)
           return f"Hello {self.name} you have deposited{amount} your new balance is{self.balance}"
      def withdraw(self,withdraw_amount):
-         total=withdraw_amount+self.transaction
+        try:
+          withdraw_amount+10
+        except TypeError:
+           return f"please enter amount in figures"
+        total=withdraw_amount+self.transaction
 
-         if withdraw_amount<=0:
+        if withdraw_amount<=0:
            return "valid amount"
 
-         elif total<=self.balance:
+        elif total<=self.balance:
            return "insufficient amount"
        
-         else:
-           self.balance==total
-           transction={
+        else:
+          self.balance==total
+          transction={
             "withdraw_amount":withdraw_amount,"balance":self.balance,"narration":"You have withdrawn",
             "time":datetime.now(),}
           
-           self.transactions.append(transction)
-           return f"Hello {self.name} you have deposited{withdraw_amount} your new balance is{self.balance}"
+          self.transactions.append(transction)
+          return f"Hello {self.name} you have deposited{withdraw_amount} your new balance is{self.balance}"
            
      def borrow(self,borrow_amount) :
-         loan=borrow_amount- self.loanlimit
-         loanfees=0.05*borrow_amount
-         if borrow_amount>0: 
-             return "Not quallified for the loan"
-         elif loan<=34000 :    
+        try:
+          borrow_amount+10
+        except TypeError:
+          return f"please enter amount in figures"
+
+        loan=borrow_amount- self.loanlimit
+        loanfees=0.05*borrow_amount
+        if borrow_amount>0: 
+          return " quallified for the loan"
+        elif loan<=34000 :    
              return "try again later"  
-         else :
+        else :
              self.balance=loan
              transction={
             "borrow_amount":borrow_amount,"balance":self.balance,"narration":"You have borrowed",
@@ -60,6 +74,7 @@ class Account:
              return f"Hi {self.name} you are not allowed to borrow{borrow_amount} since you outstanding balance is{self.balance}" 
 
      def get_statement(self):
+
        for transaction in self.transactions:
          amount=transaction["amount"]
          narration=transaction["narration"]
@@ -70,10 +85,16 @@ class Account:
      
 
      def repay_loan(self,amount):
+       try:
+         amount+10
+       except TypeError:
+          return f"please enter amount in figures"
+
        
-        if amount<0:
+       
+       if amount<0:
           return "you  are not allowed to pay"
-        elif amount> self.loan:
+       elif amount> self.loan:
           minus=amount-self.loan
           self.balance+=minus
           transction={
@@ -82,14 +103,53 @@ class Account:
           
           self.transactions.append(transction)
           return "you are awesome"
-        else:
+       else:
           self.loan-=amount
+          self.loan=0
           transction={
           "borrow_amount":amount,"balance":self.balance,"narration":"You have successfully repaid",
           "time":datetime.now(),}
           
           self.transactions.append(transction)
           return "you have an existing loan balance"
+
+     def transfer(self,amount,account):
+        try:
+         amount+10
+        except TypeError:
+          return f"please enter amount in figures"
+        amount>0
+        loanfee=amount*0.05
+        total=amount+loanfee
+        if total>self.balance:
+          return f"your balances is {self.balance}  you need{total} inorder to transfer"
+        else:
+          self.balance=total
+          account.deposite(amount)
+          return f"you have sent{amount} to{account.name} and your balance is{self.balance}"
+class MobileMoneyAccount(Account):
+     def __init__(self,name,phonenumber,service_provider):
+        Account.__init__(self,name,phonenumber)
+        self.service_provider=service_provider
+
+     def buy_airtime(self,amount):
+       amount>0
+       amount<=self.balance
+       amount-=self.balance
+       transction={
+            "withdraw_amount":amount,"balance":self.balance,"narration":"You have withdrawn",
+            "time":datetime.now(),}
+          
+       self.transactions.append(transction)
+       return f"Hello {self.name} confirm that you have bought airtime of{amount}  and your new balance is{self.balance}"
+           
+
+
+
+
+       
+       
+    
 
          
 
